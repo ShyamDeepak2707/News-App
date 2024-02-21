c66d1808f5d76c0a538bcf851826283e08fe9a31
Shyam Deepak <shyamdeepak2707@gmail.com>
2024-02-21 22:06:38 +0530

    initial commit

--- src/NavBar.js
+++ src/NavBar.js
@@ -7,27 +7,27 @@ export default class NavBar extends Component {
             <div>
                 <nav className="navbar navbar-expand-lg bg-body-tertiary">
                     <div className="container-fluid">
-                        <Link className="navbar-brand" to="/">OnlineTimes</Link>
+                        <Link className="navbar-brand" to="News-App/">OnlineTimes</Link>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                                 <li className="nav-item">
-                                    <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                                    <Link className="nav-link active" aria-current="page" to="News-App/">Home</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <Link className="nav-link" to="/business">Business</Link></li>
+                                    <Link className="nav-link" to="News-App/business">Business</Link></li>
                                 <li className="nav-item">
-                                    <Link className="nav-link" to="/entertainment">Entertainment</Link></li>
+                                    <Link className="nav-link" to="News-App/entertainment">Entertainment</Link></li>
                                 <li className="nav-item">
-                                    <Link className="nav-link" to="/health">Health</Link></li>
+                                    <Link className="nav-link" to="News-App/health">Health</Link></li>
                                 <li className="nav-item">
-                                    <Link className="nav-link" to="/science">Science</Link></li>
+                                    <Link className="nav-link" to="News-App/science">Science</Link></li>
                                 <li className="nav-item">
-                                    <Link className="nav-link" to="/sports">Sports</Link></li>
+                                    <Link className="nav-link" to="News-App/sports">Sports</Link></li>
                                 <li className="nav-item">
-                                    <Link className="nav-link" to="/technology">Technology</Link>
+                                    <Link className="nav-link" to="News-App/technology">Technology</Link>
                                 </li>
                             </ul>
                         </div>
